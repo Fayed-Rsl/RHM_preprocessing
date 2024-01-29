708c82ebb69e59a1d4cb7c39fedf362769a8ed20
Fayed Test <rasfay01@iknana-ldeb-01.vmed.uk-d.de>
2024-01-29 16:39:11 +0100

    Remove padding and do interpolate instead

--- run_coherence_average.py
+++ run_coherence_average.py
@@ -10,8 +10,7 @@ import os
 import pickle
 import pandas as pd 
 # import homemade function 
-from technical_validation_utils import (parula_map, save_multipage)
-from scipy import interpolate
+from technical_validation_utils import (parula_map, interp, save_multipage)
 
 mne.set_log_level(verbose='CRITICAL') # reduce verbose output
 # %matplotlib qt
@@ -86,61 +85,12 @@ for n_sub, subject_dir in enumerate(coherence_dir):
     all_coherences_left.append(np.mean(coherences_left, axis=0))
     all_coherences_right.append(np.mean(coherences_right, axis=0))
 
- 
 # %%
-# create a function to pad the grand average.sometime the shape of MEG sensors is 204 or 202.
-# here we reshape the sensors column to the maximum size of the array so that every coherence have 204 sensors.
-# we fill the extra array with nan when it does not already exist. (i.e. if 202 it will be 204 and fill the extra with nan)
-def pad(grand_ave):
-    array = np.array(grand_ave, dtype=object)
-    max_sensors = max(matrix.shape[0] for matrix in array)
-    padded = [np.pad(matrix, ((0, max_sensors - matrix.shape[0]), (0, 0)), mode='constant', constant_values=np.nan)
-              for matrix in grand_ave]
-    return np.array(padded)
-
-def interp(grand_ave):
-    # interpolate function to have a list of interpolated coherence matrices with the same size
-    array = np.array(grand_ave, dtype=object)
-    # find the maximum number of sensors across all subjects
-    max_sensors = max(matrix.shape[0] for matrix in array)
-    
-    # initialize a list to store interpolated coherence matrices
-    interpolated_coherence = []
-    
-    # interpolate coherence matrices to have the same number of sensors
-    for c in array:
-        n_sensors = c.shape[0]
-        
-        # interpolate along the sensor axis
-        x = np.arange(n_sensors)
-        f = interpolate.interp1d(x, c, axis=0, kind='linear', fill_value='extrapolate')
-        
-        # create new axis for interpolated sensors
-        new_x = np.linspace(0, n_sensors - 1, max_sensors)
-        
-        # interpolate coherence matrix
-        interpolated = f(new_x)
-        interpolated_coherence.append(interpolated)
-        
-    # average over all subjects axis
-    inter_grand_ave = np.mean(interpolated_coherence, axis=0) 
-    return inter_grand_ave
-# %%
-# average on subject axis and average while ignoring the nan we just created to have equal shape for al
-# pad_grand_ave_coh = np.nanmean(pad(all_coherences), axis=0)
-# pad_grand_ave_coh_left = np.nanmean(pad(all_coherences_left), axis=0)
-# pad_grand_ave_coh_right = np.nanmean(pad(all_coherences_right), axis=0)
-
-# # internally topomap function uses ascale of 1e13**2 in the data because units is grad
-# # cancel out this scaling to keep our actual coherence values 
+# internally topomap function uses ascale of 1e13**2 in the data because units is grad
+# cancel out this scaling to keep our actual coherence values 
 scaling = 1e13 ** 2
 
-# padding
-# grand_ave_coh = (pad_grand_ave_coh / scaling)
-# grand_ave_coh_left = (pad_grand_ave_coh_left / scaling)
-# grand_ave_coh_right = (pad_grand_ave_coh_right / scaling)
-
-# interp
+# interpolate missing sensors and rescale it for the coherences list 
 grand_ave_coh = (interp(all_coherences) / scaling)
 grand_ave_coh_left = (interp(all_coherences_left) / scaling)
 grand_ave_coh_right = (interp(all_coherences_right)/ scaling)
